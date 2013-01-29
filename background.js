e01622d5b7ecb8ae18bf33a569ecee74295a8e48
Dominic Hamon <dominic@measurementlab.net>
2013-01-29 12:23:02 -0800

    Reference generated index

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+  chrome.app.window.create('_index.html.html', {
     'width': 400,
     'height': 500
   });
