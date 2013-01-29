b0633198199f0067c938f8e1ab5f731cc259d895
Dominic Hamon <dominic@measurementlab.net>
2013-01-29 12:22:14 -0800

    Compiling

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 $DART_SDK/bin/pub update
 $DART_SDK/bin/dart --package-root=packages/ packages/web_ui/dwc.dart index.html
-#$DART_SDK/bin/dart2js ./chrooniprobe.dart -ochrooniprobe.dart.js --disallow-unsafe-eval
+$DART_SDK/bin/dart2js _index.html_bootstrap.dart -o_index.html_bootstrap.dart.js --disallow-unsafe-eval
--- chrooniprobe.dart
+++ chrooniprobe.dart
@@ -1,8 +1,17 @@
+import 'dart:html';
+import 'package:web_ui/watcher.dart' as watchers;
+
 String currentTime;
 
 void main() {
+  window.setInterval(updateTime, 1000);
+  updateTime();
+}
+
+void updateTime() {
   Date today = new Date.now();
   currentTime = formatTime(today.hour, today.minute, today.second);
+  watchers.dispatch();
 }
 
 String formatTime(int h, int m, int s) {
