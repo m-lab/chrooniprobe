c48fdfaf6c37a79dce1008626cbcae42af7f5606
Dominic Hamon <dominic@measurementlab.net>
2013-01-31 15:29:58 -0800

    Adding UI elements

--- chrooniprobe.dart
+++ chrooniprobe.dart
@@ -2,10 +2,38 @@ import 'dart:html';
 import 'package:web_ui/watcher.dart' as watchers;
 
 String currentTime;
+String testResolver;
+String testSite;
+
+ButtonElement startDNSTamperButton;
+ButtonElement startHTTPRequestsButton;
 
 void main() {
+  startDNSTamperButton = query("#start-dnstamper");
+  startHTTPRequestsButton = query("#start-httprequests");
+
   window.setInterval(updateTime, 1000);
   updateTime();
+}
+
+void startDNSTamper() {
+  var results = query('#results-dnstamper');
+  results.innerHtml.clear();
+  setButtonStates(false);
+  window.setInterval(function() {
+    results.innerHtml = 'Ran against $testResolver';
+    setButtonStates(true);
+  }, 3000);
+}
+
+void startHTTPRequests() {
+  var results = query('#results-httprequests');
+  results.innerHtml.clear();
+  setButtonStates(false);
+  window.setInterval(function() {
+    results.innerHtml = 'Ran against $testSite';
+    setButtonStates(true);
+  }, 3000);
 }
 
 void updateTime() {
@@ -19,4 +47,9 @@ String formatTime(int h, int m, int s) {
   String minute = (m <= 9) ? '0$m' : '$m';
   String second = (s <= 9) ? '0$s' : '$s';
   return '$hour:$minute:$second';
+}
+
+void setButtonStates(bool enabled) {
+  startDNSTamperButton.disabled = !enabled;
+  startHTTPRequestsButton.disabled = !enabled;
 }
