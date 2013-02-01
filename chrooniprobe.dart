d41b0082c84648728bb94e25b842d13ba550c88e
Dominic Hamon <dominic@measurementlab.net>
2013-02-01 11:14:50 -0800

    Added framework for TCP Connect test

--- chrooniprobe.dart
+++ chrooniprobe.dart
@@ -3,9 +3,11 @@ import 'package:web_ui/watcher.dart' as watchers;
 
 final String DEFAULT_RESOLVER = '8.8.8.8';
 final String DEFAULT_SITE = 'http://www.google.com';
+final String DEFAULT_IPS = '8.8.8.8:53\n8.8.4.4:53';
 
 String testResolver;
 String testSite;
+String testIPs;
 
 class OONITest {
   OONITest(this.name, this.do_test) {
@@ -28,12 +30,19 @@ class OONITest {
 
 List<OONITest> tests = [
   new OONITest('dnstamper', (results) {
-      if (testResolver == null) testResolver = DEFAULT_RESOLVER;
+      if (testResolver == null || testResolver.isEmpty) testResolver = DEFAULT_RESOLVER;
       results.innerHtml = 'Ran against $testResolver';
   }),
   new OONITest('httprequests', (results) {
-      if (testSite == null) testSite = DEFAULT_SITE;
+      if (testSite == null || testSite.isEmpty) testSite = DEFAULT_SITE;
       results.innerHtml = 'Ran against $testSite';
+  }),
+  new OONITest('tcpconnect', (results) {
+      if (testIPs == null || testIPs.isEmpty) testIPs = DEFAULT_IPS;
+      var ipList = testIPs.split('\n');
+      ipList.forEach((testIP) {
+          results.innerHtml = results.innerHtml.concat('Ran against $testIP<br/>');
+      });
   })
 ];
 
